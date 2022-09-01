61edb014019122a88eaa47e98d64d1a98049188b
Anya Bu <annabukhtiiarova@gmail.com>
2022-09-01 19:36:22 +0300

    modified

--- src/index.js
+++ src/index.js
@@ -1,23 +1,54 @@
 import './scss/style.scss';
 
-async function check() {
-  await fetch ('https://google.com');
-}
+document.addEventListener('DOMContentLoaded', (event) => {
 
-check().then(()=> {
-  console.log('success');
-}).catch(()=> {
-  console.log('error');
+  const menuBtn = document.querySelector('.hero__mobile-menu-btn');
+  const menu = document.querySelector('.hero__menu-mobile');
+  const menuMobileList = document.querySelector('.hero__menu-list-mobile');
+  const crossBtn = document.querySelector('.hero__mobile-menu-btn-cross');
+
+
+  if (menuBtn && crossBtn) {
+
+    menuBtn.addEventListener('click', () => {
+      menu.classList.add('active');
+      menuBtn.classList.add('active');
+    })
+
+    crossBtn.addEventListener('click', () => {
+      menu.classList.remove('active');
+      menuBtn.classList.remove('active');
+    })
+  }
+
+  menuMobileList.addEventListener('click', () => {
+    menu.classList.remove('active');
+    menuBtn.classList.remove('active');
+  })
+
+})
+
+document.addEventListener('DOMContentLoaded', (event) => {
+
+  const menuList = document.querySelector('.header__nav-list');
+  const menuLinks = document.getElementsByClassName('header__nav-item-link')
+
+  menuList.addEventListener('click', (item) => {
+    [...menuLinks].forEach(function (element) {
+      if (element.classList.contains('active')) {
+        element.classList.remove('active')
+      }
+    })
+
+    item.target.classList.add('active')
+  })
+
 })
 
 
-let menuBtn = document.querySelector('.hero__mobile-menu-btn');
-let menu = document.querySelector('.hero__menu-mobile');
-let bars = document.querySelectorAll('.hero__mobile-menu-bar')
-menuBtn.addEventListener('click', function(){
-  console.log(bars)
-  menu.classList.toggle('active');
-  menuBtn.classList.toggle('active')
-  bars[0].classList.toggle('change');
-  bars[2].classList.toggle('change');
-})
+
+
+
+
+
+
